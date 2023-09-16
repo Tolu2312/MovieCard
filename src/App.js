f2aec4333c132b9b8df3160a149146a6ef9ad5af
Tolu2312 <agbooladavid9@gmail.com>
2023-09-16 14:49:53 +0100

    create movie card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
+import MovieList from './components/MovieList';
+import Filter from './components/Filter';
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      title: 'The kidnapping day',
+      description: 'A clumsy kidnapper kidnaps a genius girl!',
+      posterURL: 'https://www.thenetnaija.net/videos/kdrama/19611-the-kidnapping-day',
+      rating: 4.5,
+    },
+    {
+      title: 'Han River Police',
+      description: 'A policeman and his team handle different incidents and accidents that occur in Han River.',
+      posterURL: ' https://www.imdb.com/title/tt28090631/',
+      rating: 4.5,
+    },
+    // Add more initial movie objects here
+  ]);
+
+  const [filters, setFilters] = useState({ title: '', rating: '' });
+
+  const handleFilterChange = (filterName, filterValue) => {
+    setFilters({ ...filters, [filterName]: filterValue });
+  };
+
+  const filteredMovies = movies.filter(
+    (movie) =>
+      movie.title.toLowerCase().includes(filters.title.toLowerCase()) &&
+      (!filters.rating || movie.rating >= parseFloat(filters.rating))
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Library</h1>
+      <Filter onFilterChange={handleFilterChange} />
+      <MovieList movies={filteredMovies} />
     </div>
   );
 }
